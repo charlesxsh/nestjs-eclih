e5f9cd7d58571f5b52a41f09d293fc158997fbcc
Shihao Xia <charlesxsh@hotmail.com>
2021-02-22 21:27:59 -0500

    removed useless deps

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,4 @@
+import { NestApplicationContextOptions } from "@nestjs/common/interfaces/nest-application-context-options.interface";
 import { NestFactory } from "@nestjs/core";
 import { CommanderService } from "./commander.service";
 
@@ -5,8 +6,13 @@ import { CommanderService } from "./commander.service";
  * Helper function for entrypoint of CLI.
  * @param appModule Nestjs Module class
  */
-export async function bootstrapCli(appModule: object) {
-    const app = await NestFactory.createApplicationContext(appModule);
+export async function bootstrapCli(appModule: object, options?:NestApplicationContextOptions) {
+    if(!options){
+        options = {
+            logger: false
+        }
+    }
+    const app = await NestFactory.createApplicationContext(appModule, options);
     const cmdSvc = app.get(CommanderService);
     return cmdSvc.startAsync();
 }
